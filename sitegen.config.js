205acddfe480d39b21e589b5a9ff1a1c3dcd5e98
Tarocch1 <huiben.fang@gmail.com>
2021-04-08 22:38:47 +0800

    build: ⬆️ 更新 sitegen 到 0.9.0

--- sitegen.config.js
+++ sitegen.config.js
@@ -7,6 +7,7 @@ dayjs.extend(timezone);
 
 module.exports = {
   title: "Tarocch1's Home Page",
+  copyright: '© 2015-present Tarocch1',
   markdown: {
     timeFormatter: time =>
       dayjs(time).tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss'),
