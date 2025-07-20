f68a49069bc943994c98fb67a24bb8a27846a2a6
Tarocch1 <huiben.fang@gmail.com>
2025-07-20 15:57:28 +0800

    feat: up

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,1 @@
-export { PrettierRules as default } from '@tarocch1/eslint-config'
+export { PrettierOptions as default } from '@tarocch1/eslint-config/prettier'
--- src/pages/Home/utils/prettier.ts
+++ src/pages/Home/utils/prettier.ts
@@ -2,33 +2,10 @@ import type { Config } from 'prettier'
 import { format } from 'prettier/standalone'
 import * as prettierPluginBabel from 'prettier/plugins/babel'
 import * as prettierPluginEstree from 'prettier/plugins/estree'
+import { PrettierOptions } from '@tarocch1/eslint-config/prettier'
 
 const defaultConfig: Config = {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: true,
-  jsxSingleQuote: false,
-  trailingComma: 'all',
-  bracketSpacing: true,
-  objectWrap: 'collapse',
-  bracketSameLine: false,
-  rangeStart: 0,
-  rangeEnd: Infinity,
-  requirePragma: false,
-  insertPragma: false,
-  checkIgnorePragma: true,
-  proseWrap: 'preserve',
-  arrowParens: 'always',
-  htmlWhitespaceSensitivity: 'ignore',
-  endOfLine: 'lf',
-  quoteProps: 'as-needed',
-  vueIndentScriptAndStyle: false,
-  embeddedLanguageFormatting: 'auto',
-  singleAttributePerLine: false,
-  experimentalOperatorPosition: 'end',
-  experimentalTernaries: false,
+  ...PrettierOptions,
   parser: 'babel',
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   plugins: [prettierPluginBabel, prettierPluginEstree as any],
